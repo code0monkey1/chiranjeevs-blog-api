4dcc370617bd73884cb2d21bf9284cd2c7073dff
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-07-20 19:29:35 +0530

    env

--- config/server.js
+++ config/server.js
@@ -1,7 +1,17 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
+  },
+  admin: {
+    // ...
+    path: "/admin",
+    build: {
+      backend: env(
+        "ADMIN_BUILD_BACKEND",
+        "https://chiranjeevs-blog-strapi-backend-api.onrender.com"
+      ),
+    },
   },
 });
